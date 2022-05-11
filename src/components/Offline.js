15c701b84d2e87db441ddd1381e8b7e925c9cf19
M Ivan Hanif V <ivanhanif03@gmail.com>
2022-05-11 12:54:24 +0700

    Offline info and Manifest

---
+++ src/components/Offline.js
@@ -0,0 +1,18 @@
+function Offline() {
+    return (
+        <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4">
+            <div className="flex">
+                <div className="flex-shrink-0">
+
+                </div>
+                <div className="ml-3">
+                    <p className="text-sm text-yellow-700">
+                        You are offline. Don't you worry, you still can do things.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Offline;
